0b782968de39bc6aab233ca903b93898c8d98287
苗志良 <13585824737@163.com>
2017-01-09 22:53:58 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,13 @@ Rails.application.routes.draw do
   end
   resources :jobs
   root 'jobs#index'
+
+  namespace :admin do
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
+  end
 end
