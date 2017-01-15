f675e8e666bac5dc4305bdfbba25a2e7541b8958
苗志良 <13585824737@163.com>
2017-01-15 21:38:28 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :admin do
-    resources :jobs
-  end
+
 
   namespace :admin do
     resources :jobs do
@@ -14,7 +12,9 @@ Rails.application.routes.draw do
       end
     end
   end
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
 
 end
